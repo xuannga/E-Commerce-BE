75e9adca2edc858add7df6c5db6eac9aa4b9bd46
xuannga <nguyenx206@gmail.com>
2021-08-04 15:15:37 -0700

    added folders and files

---
+++ models/ProductTags.js
@@ -0,0 +1,18 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class ProductTag extends Model {}
+
+ProductTag.init({
+    // define columns6
+
+}, {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+});
+
+module.exports = ProductTag;
